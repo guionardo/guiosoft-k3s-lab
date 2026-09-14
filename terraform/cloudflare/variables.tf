3bcc27e9ebd0d605706141201108854f3d4a6ab1
Guionardo Furlan <guionardo@gmail.com>
2026-09-14 11:05:04 -0300

    cloudflare: parameterize tunnel origin url

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -18,6 +18,12 @@ variable "cloudflare_tunnel_name" {
   type        = string
 }
 
+variable "cloudflare_tunnel_origin_url" {
+  description = "Origin URL shared by host and Kubernetes cloudflared replicas during migration."
+  type        = string
+  default     = "http://192.168.88.9:80"
+}
+
 variable "cloudflare_wildcard_record_id" {
   description = "Existing DNS record ID for *.guiosoft.info. Used only by import instructions."
   type        = string
